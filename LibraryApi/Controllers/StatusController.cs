876d04fd884543873978a680c57ab9779339b365
Jeffry Gonzalez <jeff@hypertheory.com>
2021-03-01 14:41:38 -0500

    Update StatusController.cs

--- LibraryApi/Controllers/StatusController.cs
+++ LibraryApi/Controllers/StatusController.cs
@@ -19,6 +19,22 @@ namespace LibraryApi.Controllers
                 LastChecked = DateTime.Now
             };
         }
+
+        // GET /customers/13
+        // GET /customers/(anyhting that is an integer)
+        [HttpGet("customers/{customerId:int}")]
+        public ActionResult GetInfoAboutCustomer(int customerId)
+        {
+            return Ok($"Getting info about customer {customerId}");
+        }
+
+        // GET blogs/2018/4/15
+        [HttpGet("blogs/{year:int}/{month:int}/{day:int}")]
+        public ActionResult GetBlogPosts(int year, int month, int day)
+        {
+            return Ok($"Getting blogs for {month}-{day}-{year}");
+        }
+
     }
 
     public class StatusResponse
