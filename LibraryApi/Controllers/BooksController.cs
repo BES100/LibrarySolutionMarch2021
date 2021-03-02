ee12985252f5a33ef5622d63126e678573485f80
Jeffry Gonzalez <jeff@hypertheory.com>
2021-03-02 14:37:57 -0500

    after dbcontext created

---
+++ LibraryApi/Controllers/BooksController.cs
@@ -0,0 +1,26 @@
+﻿using LibraryApi.Models.Books;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Controllers
+{
+    public class BooksController : ControllerBase
+    {
+
+        [HttpGet("/books/{id:int}")] 
+        public async Task<ActionResult> GetBookById(int id)
+        {
+            var response = new GetBookDetailsResponse
+            {
+                Id = id,
+                Title = "Some Title",
+                Author = "Some Author",
+                Genre = "Fiction"
+            };
+            return Ok(response);
+        }
+    }
+}
